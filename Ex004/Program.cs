eb3d73d256d0e5c44f160af2884ea127709ea87e
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-02 20:11:45 +0300

    ex004

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4
+int N = 15;
+
+int i = 0;
+int b = 0;
+while (i < N)
+{
+    i = i + 1;
+    b = i % 2;
+    if (b > 0)
+    {
+
+    }
+    else
+{
+   Console.WriteLine(i);
+}
+}
+
