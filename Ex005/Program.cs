e2e43305cd03b0c51088ec8730c4096376f60de1
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-02 20:55:03 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 5
+int N = 13;
+
+int i = 0;
+i = i - N;
+while (i <= N)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+    
---
+++ Ex006/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 6
+int N = 358;
+
+int a = N % 10;
+
+Console.WriteLine(a);
