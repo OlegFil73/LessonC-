78646552d60bae0561149befbf20307fe4735dc3
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-03 22:18:25 +0300

    lesson2.Ex002

---
+++ Lesson2/Ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1
+int More(int x)
+{
+
+    int a = x / 10;
+    int b = x % 10;
+
+    //System.Console.WriteLine($"value: {x} a:{a} b:{b}");
+    int max = a;
+    if (b > max) max = b;
+    //Console.WriteLine(max);
+    return max;
+
+}
+int value = new Random().Next(10, 100);
+System.Console.WriteLine(value);
+System.Console.WriteLine(More(value));
---
+++ Lesson2/Ex002/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2
+//int i = 0;
+//while (i++ < 10)
+{
+    int a = new Random().Next(100, 1000);
+    Console.WriteLine(a);
+
+    int b = a / 10;
+    int c = b % 10;
+    Console.WriteLine(c);
+}
