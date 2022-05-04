28bedad1b6f59633d7f222fe744894b95cb6288a
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-04 22:45:26 +0300

    Ex007

---
+++ Lesson2/Ex007/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 7
+int a = 8;
+
+int i = 10;
+if (a < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    {
+        while (a > 999)
+
+            a = a / i;
+        i = i * 10;
+    }
+
+
+    int b = a % 10;
+    Console.WriteLine(b);
+}
+
