e013715502099b31652e755c160dd6fe0d9fa247
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-13 22:37:43 +0300

    Ex002

---
+++ Lesson3/Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2
+// Напишите программу, которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+
+double distance(int ax, int ay, int bx, int by)
+{
+    int c = (ax - bx) * (ax - bx) + (ay - by) * (ay - by);
+    double result = Math.Sqrt(c);
+    return result;
+}
+double res = distance(7, -5, 1, -1);
+Console.WriteLine($"{res} расстояние между A и B");
