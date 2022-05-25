feb74105810e89b135148658dd8d1ca8f5be7b10
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-25 23:41:12 +0300

    Ex003

---
+++ Lesson4/Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.WriteLine(array[i]);
+}
+
+
+
