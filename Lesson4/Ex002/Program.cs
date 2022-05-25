9a6131c8ca4daceea57e29bdc8eca7c9b95fc08c
OlegFil73 <104713527+OlegFil73@users.noreply.github.com>
2022-05-25 22:55:21 +0300

    Ex002

---
+++ Lesson4/Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+
+
+int number = 1825;
+
+int sum = 0;
+while (number > 0)
+{
+    int temporary = number % 10;
+    sum = sum + temporary; 
+      number = number / 10;
+   }
+Console.WriteLine(sum);
